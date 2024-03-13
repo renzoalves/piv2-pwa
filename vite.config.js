82e23816c2ab0656243b20f311eb00935cac6d4b
Renzo Alves <djenrolandy@gmail.com>
2024-03-13 15:35:56 -0300

    FIX: reconfiguração do Manifesto

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,54 @@
-import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import {
+  fileURLToPath,
+  URL
+} from 'node:url'
+import {
+  defineConfig
+} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import {
+  VitePWA
+} from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Components({
-        dts: false,
+      dts: false,
     }),
-    VitePWA({ registerType: 'autoUpdate' })
-],
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'safari-pinned-tab.svg'],
+      manifest: {
+        name: "PortalImendes",
+        short_name: "PortalImendes",
+        start_url: "/",
+        display: "standalone",
+        description: 'Portal IMendes - Inovação tributária para seu Negócio. A gestão tributária de sua Empresa muito mais produtiva, segura e tranquila. Atendemos os 27 estados.',
+        theme_color: '#ffffff',
+        icons: [{
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png'
+          }
+        ]
+      },
+      devOptions: {
+        enabled: true
+      }
+    })
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src',
+        import.meta.url))
     }
   },
 
@@ -26,5 +59,5 @@ export default defineConfig({
       }
     }
   },
-  
+
 })
